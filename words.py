c8b478ecfb4e1381136cec7e7452e2f8942dd5cd
rafe <raphael.lafrance@gmail.com>
2015-12-02 19:33:10 -0500

    Improve word count program

--- words.py
+++ words.py
@@ -5,18 +5,16 @@ import re
 from collections import namedtuple
 
 base_name = 'vntraits110715'
-in_name = base_name + '_03_total_length.csv'
+in_name = base_name
 
 Row = namedtuple(
     'Row',
-    ('row_num occurrenceid institutionid collectionid datasetid '
+    ('occurrenceid institutionid collectionid datasetid '
      'institutioncode collectioncode catalognumber '
      'scientificname class_ individualcount sex lifestage '
-     'dynamicproperties occurrenceremarks fieldnotes '
-     'dwc_sex dwc_sex_source '
-     'dwc_lifeStage dwc_lifeStage_source '
-     'vto_bodyLength vto_bodyLength_units vto_bodyLength_source '
-     'vto_bodyMass vto_bodyMass_units vto_bodyMass_source'))
+     'dynamicproperties occurrenceremarks fieldnotes'))
+
+punct = re.compile(r'[\s\d!@#$%^&*()+=|\\\]\[}{\'";/?:><,]-]+')
 
 
 def main():
@@ -25,10 +23,12 @@ def main():
         row = reader.next()   # Header row
         for raw_row in reader:
             row = Row._make(raw_row)
-            words = re.split(r'\W+', row.dynamicproperties)
+            words = punct.split(row.dynamicproperties)
+            words.extend(punct.split(row.occurrenceremarks))
+            words.extend(punct.split(row.fieldnotes))
             for word in words:
                 word = word.lower()
-                if word and not re.search(r'\d', word):
+                if word:
                     print word
 
 
