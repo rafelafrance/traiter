4b5b1e63b4aade4effc5e122e3ecac8b04d45925
rafe <raphael.lafrance@gmail.com>
2015-12-16 16:26:29 -0500

    Add names to the test cases

--- test_Parse.pl
+++ test_Parse.pl
@@ -7,96 +7,97 @@ use Parse qw(
     extract_body_mass
 );
 
-my ($test, $parsed);
+my ($test, $parsed, $name);
 
 sub setup {
     my $tc = shift;
-    return { test => $tc };
+    $name = substr $tc, 0, 20;
+    return $name, { test => $tc };
 }
 
-$test = setup('"{"measurements":"308-190-45-20" }"');
+($name, $test) = setup('"{"measurements":"308-190-45-20" }"');
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'measurements');
-is($parsed->{value}, '308');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   'measurements', $name);
+is($parsed->{value}, '308', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( '308-190-45-20' );
+($name, $test) = setup( '308-190-45-20' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   '');
-is($parsed->{value}, '308');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   '', $name);
+is($parsed->{value}, '308', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( '{"measurements":"143-63-20-17=13 g" }' );
+($name, $test) = setup( '{"measurements":"143-63-20-17=13 g" }' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'measurements');
-is($parsed->{value}, '143');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   'measurements', $name);
+is($parsed->{value}, '143', $name);
+is($parsed->{units}, 'mm', $name);
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   'measurements');
-is($parsed->{value}, '13');
-is($parsed->{units}, 'g');
+is($parsed->{key},   'measurements', $name);
+is($parsed->{value}, '13', $name);
+is($parsed->{units}, 'g', $name);
 
-$test = setup( '143-63-20-17=13' );
+($name, $test) = setup( '143-63-20-17=13' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   '');
-is($parsed->{value}, '143');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   '', $name);
+is($parsed->{value}, '143', $name);
+is($parsed->{units}, 'mm', $name);
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   '');
-is($parsed->{value}, '13');
-is($parsed->{units}, '');
+is($parsed->{key},   '', $name);
+is($parsed->{value}, '13', $name);
+is($parsed->{units}, '', $name);
 
-$test = setup( 'reproductive data: Testes descended -10x7 mm; sex: male; unformatted measurements: 181-75-21-18=22 g' );
+($name, $test) = setup( 'reproductive data: Testes descended -10x7 mm; sex: male; unformatted measurements: 181-75-21-18=22 g' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   'measurements');
-is($parsed->{value}, '22');
-is($parsed->{units}, 'g');
+is($parsed->{key},   'measurements', $name);
+is($parsed->{value}, '22', $name);
+is($parsed->{units}, 'g', $name);
 
-$test = setup( '{ "massInGrams"="20.1" }' );
+($name, $test) = setup( '{ "massInGrams"="20.1" }' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   'massInGrams');
-is($parsed->{value}, '20.1');
-is($parsed->{units}, 'Grams');
+is($parsed->{key},   'massInGrams', $name);
+is($parsed->{value}, '20.1', $name);
+is($parsed->{units}, 'Grams', $name);
 
-$test = setup( 'snout-vent length=54 mm; total length=111 mm; tail length=57 mm; weight=5 g' );
+($name, $test) = setup( 'snout-vent length=54 mm; total length=111 mm; tail length=57 mm; weight=5 g' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'total length');
-is($parsed->{value}, '111');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   'total length', $name);
+is($parsed->{value}, '111', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( 'unformatted measurements=Verbatim weight=X;ToL=230;TaL=115;HF=22;E=18; ; total length=230 mm; tail length=115 mm;' );
+($name, $test) = setup( 'unformatted measurements=Verbatim weight=X;ToL=230;TaL=115;HF=22;E=18; ; total length=230 mm; tail length=115 mm;' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'total length');
-is($parsed->{value}, '230');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   'total length', $name);
+is($parsed->{value}, '230', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( '** Body length =345 cm; Blubber=1 cm ' );
+($name, $test) = setup( '** Body length =345 cm; Blubber=1 cm ' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'Body length');
-is($parsed->{value}, '345');
-is($parsed->{units}, 'cm');
+is($parsed->{key},   'Body length', $name);
+is($parsed->{value}, '345', $name);
+is($parsed->{units}, 'cm', $name);
 
-$test = setup( '762-292-121-76 2435.0g' );
+($name, $test) = setup( '762-292-121-76 2435.0g' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   '');
-is($parsed->{value}, '2435.0');
-is($parsed->{units}, 'g');
+is($parsed->{key},   '', $name);
+is($parsed->{value}, '2435.0', $name);
+is($parsed->{units}, 'g', $name);
 
-$test = setup( 'TL (mm) 44,SL (mm) 38,Weight (g) 0.77 xx' );
+($name, $test) = setup( 'TL (mm) 44,SL (mm) 38,Weight (g) 0.77 xx' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   'Weight');
-is($parsed->{value}, '0.77');
-is($parsed->{units}, 'g');
+is($parsed->{key},   'Weight', $name);
+is($parsed->{value}, '0.77', $name);
+is($parsed->{units}, 'g', $name);
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'TL');
-is($parsed->{value}, '44');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   'TL', $name);
+is($parsed->{value}, '44', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( 'Note in catalog: Mus. SW Biol. NK 30009; 91-0-17-22-62g' );
+($name, $test) = setup( 'Note in catalog: Mus. SW Biol. NK 30009; 91-0-17-22-62g' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   '');
-is($parsed->{value}, '62');
-is($parsed->{units}, 'g');
+is($parsed->{key},   '', $name);
+is($parsed->{value}, '62', $name);
+is($parsed->{units}, 'g', $name);
 
 #$test = setup( 'SVL 550 mm, tail 125 mm, 47.0 gm.' );
 #$parsed = extract_body_mass( $test, 'test');
@@ -104,185 +105,185 @@ is($parsed->{units}, 'g');
 #is($parsed->{value}, '47.0');
 #is($parsed->{units}, 'gm');
 
-$test = setup( 'body mass=20 g' );
+($name, $test) = setup( 'body mass=20 g' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   'body mass');
-is($parsed->{value}, '20');
-is($parsed->{units}, 'g');
+is($parsed->{key},   'body mass', $name);
+is($parsed->{value}, '20', $name);
+is($parsed->{units}, 'g', $name);
 
-$test = setup( 'weight=81.00 g; sex=female ? ; age=u ad.' );
+($name, $test) = setup( 'weight=81.00 g; sex=female ? ; age=u ad.' );
 $parsed = extract_sex( $test, 'test');
-is($parsed->{key},   'sex');
-is($parsed->{value}, 'female ?');
+is($parsed->{key},   'sex', $name);
+is($parsed->{value}, 'female ?', $name);
 $parsed = extract_life_stage( $test, 'test');
-is($parsed->{key},   'age');
-is($parsed->{value}, 'u ad.');
+is($parsed->{key},   'age', $name);
+is($parsed->{value}, 'u ad.', $name);
 
-$test = setup( 't.l.= 2 feet 3.1 - 4.5 inches ' );
+($name, $test) = setup( 't.l.= 2 feet 3.1 - 4.5 inches ' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   't.l.');
-is($parsed->{value}->[0], '2');
-is($parsed->{value}->[1], '3.1 - 4.5');
-is($parsed->{units}->[0], 'feet');
-is($parsed->{units}->[1], 'inches');
+is($parsed->{key},   't.l.', $name);
+is($parsed->{value}->[0], '2', $name);
+is($parsed->{value}->[1], '3.1 - 4.5', $name);
+is($parsed->{units}->[0], 'feet', $name);
+is($parsed->{units}->[1], 'inches', $name);
 
-$test = setup( '2 ft. 3.1 - 4.5 in. ' );
+($name, $test) = setup( '2 ft. 3.1 - 4.5 in. ' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   '');
-is($parsed->{value}->[0], '2');
-is($parsed->{value}->[1], '3.1 - 4.5');
-is($parsed->{units}->[0], 'ft.');
-is($parsed->{units}->[1], 'in.');
+is($parsed->{key},   '', $name);
+is($parsed->{value}->[0], '2', $name);
+is($parsed->{value}->[1], '3.1 - 4.5', $name);
+is($parsed->{units}->[0], 'ft.', $name);
+is($parsed->{units}->[1], 'in.', $name);
 
-$test = setup( 'total length= 2 ft.' );
+($name, $test) = setup( 'total length= 2 ft.' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'total length');
-is($parsed->{value}->[0], '2');
-is($parsed->{units}->[0], 'ft.');
+is($parsed->{key},   'total length', $name);
+is($parsed->{value}->[0], '2', $name);
+is($parsed->{units}->[0], 'ft.', $name);
 
-$test = setup( '2 lbs. 3.1 - 4.5 oz ' );
+($name, $test) = setup( '2 lbs. 3.1 - 4.5 oz ' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   '');
-is($parsed->{value}->[0], '2');
-is($parsed->{value}->[1], '3.1 - 4.5');
-is($parsed->{units}->[0], 'lbs.');
-is($parsed->{units}->[1], 'oz');
+is($parsed->{key},   '', $name);
+is($parsed->{value}->[0], '2', $name);
+is($parsed->{value}->[1], '3.1 - 4.5', $name);
+is($parsed->{units}->[0], 'lbs.', $name);
+is($parsed->{units}->[1], 'oz', $name);
 
-$test = setup( '{"totalLengthInMM":"x", "earLengthInMM":"20", "weight":"[139.5] g" }' );
+($name, $test) = setup( '{"totalLengthInMM":"x", "earLengthInMM":"20", "weight":"[139.5] g" }' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   'weight');
-is($parsed->{value}, '[139.5]');
-is($parsed->{units}, 'g');
+is($parsed->{key},   'weight', $name);
+is($parsed->{value}, '[139.5]', $name);
+is($parsed->{units}, 'g', $name);
 
-$test = setup( '{"fat":"No fat", "gonads":"Testes 10 x 6 mm.", "molt":"No molt", "stomach contents":"Not recorded", "weight":"94 gr."' );
+($name, $test) = setup( '{"fat":"No fat", "gonads":"Testes 10 x 6 mm.", "molt":"No molt", "stomach contents":"Not recorded", "weight":"94 gr."' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   'weight');
-is($parsed->{value}, '94');
-is($parsed->{units}, 'gr.');
+is($parsed->{key},   'weight', $name);
+is($parsed->{value}, '94', $name);
+is($parsed->{units}, 'gr.', $name);
 
-$test = setup( 'AJR-32   186-102-23-15  15.0g' );
+($name, $test) = setup( 'AJR-32   186-102-23-15  15.0g' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   '');
-is($parsed->{value}, '186');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   '', $name);
+is($parsed->{value}, '186', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( 'sex=unknown ; crown-rump length=8 mm' );
+($name, $test) = setup( 'sex=unknown ; crown-rump length=8 mm' );
 $parsed = extract_sex( $test, 'test');
-is($parsed->{key},   'sex');
-is($parsed->{value}, 'unknown');
+is($parsed->{key},   'sex', $name);
+is($parsed->{value}, 'unknown', $name);
 
-$test = setup( 'sex=unknown ; crown-rump length=8 mm' );
+($name, $test) = setup( 'sex=unknown ; crown-rump length=8 mm' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed, '');
+is($parsed, '', $name);
 
-$test = setup( 'length=8 mm' );
+($name, $test) = setup( 'length=8 mm' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'length');
-is($parsed->{value}, '8');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   'length', $name);
+is($parsed->{value}, '8', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( 'another; length=8 mm' );
+($name, $test) = setup( 'another; length=8 mm' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'length');
-is($parsed->{value}, '8');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   'length', $name);
+is($parsed->{value}, '8', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( 'another; TL_120, noise' );
+($name, $test) = setup( 'another; TL_120, noise' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'TL_');
-is($parsed->{value}, '120');
-is($parsed->{units}, '');
+is($parsed->{key},   'TL_', $name);
+is($parsed->{value}, '120', $name);
+is($parsed->{units}, '', $name);
 
-$test = setup( 'another; TL - 101.3mm, noise' );
+($name, $test) = setup( 'another; TL - 101.3mm, noise' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'TL');
-is($parsed->{value}, '101.3');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   'TL', $name);
+is($parsed->{value}, '101.3', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( 'before; TL153, after' );
+($name, $test) = setup( 'before; TL153, after' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'TL');
-is($parsed->{value}, '153');
-is($parsed->{units}, '');
+is($parsed->{key},   'TL', $name);
+is($parsed->{value}, '153', $name);
+is($parsed->{units}, '', $name);
 
-$test = setup( 'before; Total length in catalog and specimen tag as 117, after' );
+($name, $test) = setup( 'before; Total length in catalog and specimen tag as 117, after' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'Total length');
-is($parsed->{value}, '117');
-is($parsed->{units}, '');
+is($parsed->{key},   'Total length', $name);
+is($parsed->{value}, '117', $name);
+is($parsed->{units}, '', $name);
 
-$test = setup( 'before Snout vent lengths range from 16 to 23 mm. after' );
+($name, $test) = setup( 'before Snout vent lengths range from 16 to 23 mm. after' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'Snout vent lengths');
-is($parsed->{value}, '16 to 23');
-is($parsed->{units}, 'mm.');
+is($parsed->{key},   'Snout vent lengths', $name);
+is($parsed->{value}, '16 to 23', $name);
+is($parsed->{units}, 'mm.', $name);
 
-$test = setup( 'Size=13 cm TL' );
+($name, $test) = setup( 'Size=13 cm TL' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'TL');
-is($parsed->{value}, '13');
-is($parsed->{units}, 'cm');
+is($parsed->{key},   'TL', $name);
+is($parsed->{value}, '13', $name);
+is($parsed->{units}, 'cm', $name);
 
-$test = setup( 'det_comments:31.5-58.3inTL' );
+($name, $test) = setup( 'det_comments:31.5-58.3inTL' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'TL');
-is($parsed->{value}, '31.5-58.3');
-is($parsed->{units}, 'in');
+is($parsed->{key},   'TL', $name);
+is($parsed->{value}, '31.5-58.3', $name);
+is($parsed->{units}, 'in', $name);
 
-$test = setup( 'SVL52mm' );
+($name, $test) = setup( 'SVL52mm' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'SVL');
-is($parsed->{value}, '52');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   'SVL', $name);
+is($parsed->{value}, '52', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( 'snout-vent length=221 mm; total length=257 mm; tail length=36 mm' );
+($name, $test) = setup( 'snout-vent length=221 mm; total length=257 mm; tail length=36 mm' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'total length');
-is($parsed->{value}, '257');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   'total length', $name);
+is($parsed->{value}, '257', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( 'SVL 209 mm, total 272 mm, 4.4 g.' );
+($name, $test) = setup( 'SVL 209 mm, total 272 mm, 4.4 g.' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed->{key},   'total');
-is($parsed->{value}, '272');
-is($parsed->{units}, 'mm');
+is($parsed->{key},   'total', $name);
+is($parsed->{value}, '272', $name);
+is($parsed->{units}, 'mm', $name);
 
-$test = setup( 'Note in catalog: 83-0-17-23-fa64-35g' );
+($name, $test) = setup( 'Note in catalog: 83-0-17-23-fa64-35g' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   '');
-is($parsed->{value}, '35');
-is($parsed->{units}, 'g');
+is($parsed->{key},   '', $name);
+is($parsed->{value}, '35', $name);
+is($parsed->{units}, 'g', $name);
 
-$test = setup( '{"measurements":"20.2g, SVL 89.13mm" }' );
+($name, $test) = setup( '{"measurements":"20.2g, SVL 89.13mm" }' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   'measurements');
-is($parsed->{value}, '20.2');
-is($parsed->{units}, 'g');
+is($parsed->{key},   'measurements', $name);
+is($parsed->{value}, '20.2', $name);
+is($parsed->{units}, 'g', $name);
 
-$test = setup( 'Body: 15 g' );
+($name, $test) = setup( 'Body: 15 g' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   'Body');
-is($parsed->{value}, '15');
-is($parsed->{units}, 'g');
+is($parsed->{key},   'Body', $name);
+is($parsed->{value}, '15', $name);
+is($parsed->{units}, 'g', $name);
 
-$test = setup( '82-00-15-21-tr7-fa63-41g' );
+($name, $test) = setup( '82-00-15-21-tr7-fa63-41g' );
 $parsed = extract_body_mass( $test, 'test');
-is($parsed->{key},   '');
-is($parsed->{value}, '41');
-is($parsed->{units}, 'g');
+is($parsed->{key},   '', $name);
+is($parsed->{value}, '41', $name);
+is($parsed->{units}, 'g', $name);
 
-$test = setup( 'left gonad length=10 mm; right gonad length=10 mm;' );
+($name, $test) = setup( 'left gonad length=10 mm; right gonad length=10 mm;' );
 $parsed = extract_total_length( $test, 'test');
-is($parsed, '');
+is($parsed, '', $name);
 
-$test = setup( 'Respective sex and msmt. in mm' );
+($name, $test) = setup( 'Respective sex and msmt. in mm' );
 $parsed = extract_sex( $test, 'test');
-is($parsed, '');
+is($parsed, '', $name);
 
-$test = setup( 'sex=unknown ; age class=adult/juvenile' );
+($name, $test) = setup( 'sex=unknown ; age class=adult/juvenile' );
 $parsed = extract_life_stage( $test, 'test');
-is($parsed->{key},   'age class');
-is($parsed->{value}, 'adult/juvenile');
+is($parsed->{key},   'age class', $name);
+is($parsed->{value}, 'adult/juvenile', $name);
 
 
 done_testing();
