2e2f9d283b9da90cccba2ffab708280b4d15d079
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2016-04-21 12:06:30 -0400

    Add tests for preferred_or_search

--- lib/tests/test_life_stage_parser.py
+++ lib/tests/test_life_stage_parser.py
@@ -39,6 +39,46 @@ class TestLifeStageParser(unittest.TestCase):
             target.parse('LifeStage Remarks: 5-6 wks'),
             {'key': 'LifeStage Remarks', 'value': '5-6 wks'})
 
+    ######################################################################
+    ######################################################################
+    ######################################################################
+    ######################################################################
+
+    def test_preferred_or_search_1(self):
+        self.assertDictEqual(
+            target.preferred_or_search('', ['sex=unknown ; age class=adult/juvenile']),
+            {'derivedLifeStage': 'adult/juvenile', 'hasLifeStage': 1})
+
+    def test_preferred_or_search_2(self):
+        self.assertDictEqual(
+            target.preferred_or_search('', ['weight=81.00 g; sex=female ? ; age=u ad.']),
+            {'derivedLifeStage': 'u ad.', 'hasLifeStage': 1})
+
+    def test_preferred_or_search_3(self):
+        self.assertDictEqual(
+            target.preferred_or_search('', ['weight=5.2 g; age class=over-winter ; total length=99 mm;']),
+            {'derivedLifeStage': 'over-winter', 'hasLifeStage': 1})
+
+    def test_preferred_or_search_4(self):
+        self.assertDictEqual(
+            target.preferred_or_search('', ['sex=female ? ; age=1st year more than four words here']),
+            {'derivedLifeStage': '1st year', 'hasLifeStage': 1})
+
+    def test_preferred_or_search_5(self):
+        self.assertDictEqual(
+            target.preferred_or_search('', ['words after hatching year more words']),
+            {'derivedLifeStage': 'after hatching year', 'hasLifeStage': 1})
+
+    def test_preferred_or_search_6(self):
+        self.assertEqual(
+            target.preferred_or_search('', ['age determined by 20-sided die']),
+            {'derivedLifeStage': '', 'hasLifeStage': 0})
+
+    def test_preferred_or_search_7(self):
+        self.assertDictEqual(
+            target.preferred_or_search('', ['LifeStage Remarks: 5-6 wks']),
+            {'derivedLifeStage': '5-6 wks', 'hasLifeStage': 1})
+
 
 target = LifeStageParser()
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestLifeStageParser)
--- lib/tests/test_sex_parser.py
+++ lib/tests/test_sex_parser.py
@@ -34,6 +34,41 @@ class TestSexParser(unittest.TestCase):
             target.parse('Respective sex and msmt. in mm'),
             None)
 
+    ######################################################################
+    ######################################################################
+    ######################################################################
+    ######################################################################
+
+    def test_preferred_or_search_1(self):
+        self.assertDictEqual(
+            target.preferred_or_search('', ['weight=81.00 g; sex=female ? ; age=u ad.']),
+            {'derivedSex': 'female ?', 'hasSex': 1})
+
+    def test_preferred_or_search_2(self):
+        self.assertDictEqual(
+            target.preferred_or_search('', ['sex=unknown ; crown-rump length=8 mm']),
+            {'derivedSex': 'unknown', 'hasSex': 1})
+
+    def test_preferred_or_search_3(self):
+        self.assertDictEqual(
+            target.preferred_or_search('', ['sex=F crown rump length=8 mm']),
+            {'derivedSex': 'F', 'hasSex': 1})
+
+    def test_preferred_or_search_4(self):
+        self.assertDictEqual(
+            target.preferred_or_search('', ['words male female unknown more words']),
+            {'derivedSex': 'male,female', 'hasSex': 1})
+
+    def test_preferred_or_search_5(self):
+        self.assertEqual(
+            target.preferred_or_search('', ['words male female male more words']),
+            {'derivedSex': '', 'hasSex': 0})
+
+    def test_preferred_or_search_6(self):
+        self.assertEqual(
+            target.preferred_or_search('', ['Respective sex and msmt. in mm']),
+            {'derivedSex': '', 'hasSex': 0})
+
 
 target = SexParser()
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestSexParser)
