c599c63d8ce5de3f39183e24fa8d49dce46987a7
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2018-10-09 14:48:53 -0400

    WIP update repository structure

---
+++ traiter/traiter.py
@@ -0,0 +1,35 @@
+import sys
+import csv
+from trait_parsers.body_mass_parser import BodyMassParser
+from trait_parsers.life_stage_parser import LifeStageParser
+from trait_parsers.sex_parser import SexParser
+from trait_parsers.total_length_parser import TotalLengthParser
+
+
+class Traiter:
+
+    def __init__(self):
+        self.body_mass_parser    = BodyMassParser()
+        self.life_stage_parser   = LifeStageParser()
+        self.sex_parser          = SexParser()
+        self.total_length_parser = TotalLengthParser()
+
+    def parse_row(self, row):
+        strings = [row['dynamicproperties'], row['occurrenceremarks'], row['fieldnotes']]
+        traits  = self.sex_parser.preferred_or_search(row['sex'], strings)
+        traits.update(self.life_stage_parser.preferred_or_search(row['lifestage'], strings))
+        traits.update(self.total_length_parser.search_and_normalize(strings))
+        traits.update(self.body_mass_parser.search_and_normalize(strings))
+        return traits
+
+    def parse_csv_file(self, file_name):
+        with open(file_name, 'r') as in_file:
+            reader = csv.DictReader(in_file)
+            for row in reader:
+                traits = self.parse_row(row)
+                print(reader.line_num)
+
+
+if __name__ == "__main__":
+    traiter = Traiter()
+    traiter.parse_csv_file(sys.argv[1])
