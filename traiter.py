390a35dddf4ca212c1f003b973ac367683c232db
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2018-11-20 16:05:49 -0500

    Add HTML output for the parser results WIP

--- traiter.py
+++ traiter.py
@@ -8,7 +8,8 @@ import csv
 import json
 import argparse
 import textwrap
-from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+from jinja2 import Environment, FileSystemLoader, Template
 from lib.trait_parsers.sex import ParseSex
 from lib.trait_parsers.body_mass import ParseBodyMass
 from lib.trait_parsers.life_stage import ParseLifeStage
@@ -28,6 +29,9 @@ TRAITS = [
     ('testes_state', ParseTestesState)]
 TRAIT_NAMES = ', '.join([i[0] for i in TRAITS])
 
+TEMPLATE = Template(
+    '{{ left }}<span class="found">{{ middle }}</span>{{ right }}')
+
 
 def parse_csv_file(args):
     """Build all of the parsers."""
@@ -35,70 +39,114 @@ def parse_csv_file(args):
                if trait in args.traits]
 
     writer = None
+    totals = {'rows': 0, 'empty': 0}
 
     reader = csv.DictReader(args.infile)
     for (i, in_row) in enumerate(reader, 1):
+        totals['rows'] += 1
 
         if not i % 1000:
             print(i)
 
         if writer is None:
             # Setup the output file
-            input_fields = get_input_fields(args, parsers, reader)
-            parsed_fields = [f'parsed_{t}' for (t, p) in parsers]
-            writer = output_header(args, input_fields, parsed_fields)
+            columns = reader.fieldnames
+            preferred_columns = [p.preferred_value for (t, p) in parsers
+                                 if p.preferred_value
+                                 and p.preferred_value in columns]
+            input_columns = (args.columns + preferred_columns
+                             + args.extra_columns)
+            parsed_columns = [f'parsed_{t}' for (t, p) in parsers]
+
+            for col in parsed_columns:
+                totals[col] = 0
+
+            all_columns = parsed_columns + input_columns
+            writer = output_start(args, all_columns)
 
         strings = [in_row[col] for col in args.columns]
-        out_row = {c: in_row[c] for c in input_fields}
+
+        if not sum(len(s) for s in strings):
+            totals['empty'] += 1
+            continue
+
+        out_row = {c: in_row[c] for c in input_columns}
 
         for trait, parser in parsers:
-            column = f'parsed_{trait}'
-
             preferred_value = ''
             if parser.preferred_value:
                 preferred_value = in_row.get(parser.preferred_value, '')
 
             parsed = parser.parser(strings, preferred_value)
 
-            out_row[column] = json.dumps(parsed)
+            column = f'parsed_{trait}'
+            out_row[column] = output_cell(args, parsed, out_row)
+            totals[column] += int(parsed['found'])
 
         output_row(args, writer, out_row)
 
-    output_footer(args, writer)
+    output_end(args, writer, preferred_columns, totals)
 
 
-def get_input_fields(args, parsers, reader):
-    """Get field names (w/out traits) from the arguments & input file."""
-    fieldnames = args.extra_columns + args.columns
-    columns = reader.fieldnames
-    fieldnames += [p.preferred_value for (t, p) in parsers
-                   if p.preferred_value and p.preferred_value in columns]
-    return fieldnames
-
-
-def output_header(args, input_fields, parsed_fields):
+def output_start(args, all_columns):
     """Output the report header."""
+    # Handle HTML output
     if args.html:
         return []
 
-    writer = csv.DictWriter(
-        args.outfile, fieldnames=(input_fields + parsed_fields))
+    # Handle CSV output
+    writer = csv.DictWriter(args.outfile, fieldnames=all_columns)
     writer.writeheader()
     return writer
 
 
 def output_row(args, writer, out_row):
     """Output a report row."""
+    # Handle HTML output
     if args.html:
+        writer.append(out_row)
         return
 
+    # Handle CSV output
     writer.writerow(out_row)
 
 
-def output_footer(args, writer):
+def output_cell(args, parsed, row):
+    """Output a report cell."""
+    # Handle CSV output
+    if not args.html:
+        return json.dumps(parsed)
+
+    # Handle HTML output
+    if parsed['found']:
+        field_name = parsed['field']
+        field = row[field_name]
+        start = parsed['start']
+        end = parsed['end']
+        left = field[:start] if start else ''
+        middle = field[start:end]
+        right = field[end:] if end <= len(field) else ''
+        row[field_name] = TEMPLATE.render(
+            left=left, middle=middle, right=right)
+    return parsed
+
+
+def output_end(args, writer, preferred_columns, totals):
     """Output the report footer."""
+    # Handle CSV output
     if not args.html:
         return
+
+    # Handle HTML output
+    env = Environment(loader=FileSystemLoader('./templates'))
+    template = env.get_template('traiter.html')
+    report = template.render(
+        args=vars(args),
+        now=datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M'),
+        rows=writer,
+        preferred_columns=preferred_columns,
+        totals=totals)
+    args.outfile.write(report)
 
 
 def parse_args():
@@ -125,7 +173,7 @@ def parse_args():
                             '{TRAIT_NAMES}'.""")
 
     parser.add_argument('--extra-columns', '-e', default='',
-                        help="""A comma separated list of any extra fields to
+                        help="""A comma separated list of any extra columns to
                             append to an output row. You may need to quote this
                             argument.""")
 
