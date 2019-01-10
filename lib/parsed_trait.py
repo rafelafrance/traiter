d2072c5fd936625ae120b8d7f945dfe6abb5b80a
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2019-01-10 00:23:19 -0500

    Remove dead code

--- lib/parsed_trait.py
+++ lib/parsed_trait.py
@@ -20,23 +20,17 @@ class ParsedTrait:
         self.start = start
         self.end = end
         self.flags = flags if flags else {}
-        self._old_value = None
-
-    def filtered_dict(self):
-        """Remove hidden attributes from the __dict__."""
-        return {k: v for k, v in self.__dict__.items() if k[0] != '_'}
 
     def __repr__(self):
         """Represent the result."""
-        return 'ParsedTrait({})'.format(self.filtered_dict())
+        return 'ParsedTrait({})'.format(self.__dict__)
 
     def __eq__(self, other):
         """Compare results."""
-        return self.filtered_dict() == other.filtered_dict()
+        return self.__dict__ == other.__dict__
 
     def convert_value(self, units):
         """Set the units and convert_value the value."""
-        self._old_value = self.value
         self.units = units
         if not units:
             self.flags['units_inferred'] = True
@@ -47,12 +41,6 @@ class ParsedTrait:
                 self.value = [convert(v, u) for v, u in zip(self.value, units)]
             else:
                 self.value = convert(self.value, units)
-
-    def unset_units(self):
-        """Rollback setting of units."""
-        self.units = None
-        self.value = self._old_value
-        self.flags['units_inferred'] = True
 
     @staticmethod
     def to_float(value):
