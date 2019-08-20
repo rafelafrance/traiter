fba3c25df4bded6454b6137cf307d167d7787c65
rafe <raphael.lafrance@gmail.com>
2019-08-20 15:18:26 -0400

    Add more nipple count trait forms

--- lib/trait_builders/nipple_count_trait_builder.py
+++ lib/trait_builders/nipple_count_trait_builder.py
@@ -23,11 +23,13 @@ class NippleCountTraitBuilder(NumericTraitBuilder):
         """Define the tokens."""
         self.shared_token(r_tkn.nipple)
         self.shared_token(tkn.integer)
+        self.shared_token(r_tkn.visible)
 
         self.fragment('op', r' [+:] ')
         self.fragment('eq', r' [=] ')
 
-        self.keyword('modifier', r' inguinal ')
+        self.keyword('adj', r"""
+            inguinal ing pectoral pec pr """.split())
 
         self.keyword('none', r""" no | none """)
 
@@ -38,33 +40,52 @@ class NippleCountTraitBuilder(NumericTraitBuilder):
         """Define rules for token simplification."""
         self.replace('count', ' integer | none ')
 
+        self.replace('modifier', 'adj visible'.split())
+
     def build_product_rules(self):
         """Define rules for output."""
+        self.product(self.typed, [
+            """ (?P<notation> 
+                    (?P<value1> count) modifier
+                    (?P<value2> count) modifier
+                ) nipple """,
+        ])
+
         self.product(self.convert, [
+
             # Eg: 1:2 = 6 mammae
-            """ (?P<notation> count (op)? count
-                ((eq)? (?P<count> count))? ) nipple
-                """,
+            """ nipple (op)? 
+                (?P<notation> count (modifier)? 
+                    (op)? count (modifier)?
+                    ((eq) (?P<value> count))? ) """,
+
+            # Eg: 1:2 = 6 mammae
+            """ (?P<notation> count (modifier)? (op)? count (modifier)?
+                ((eq) (?P<value> count))? ) nipple """,
 
             # Eg: 6 mammae
-            """ (?P<count> count ) (modifier)? nipple """,
+            """ (?P<value> count ) (modifier)? nipple """,
 
             # Eg: nipples 5
-            """ nipple (?P<count> count ) """,
+            """ nipple (?P<value> count ) """,
         ])
 
     @staticmethod
     def convert(token):
         """Convert single value tokens into a result."""
+        if not token.groups.get('value'):
+            return None
         trait = NumericTrait(start=token.start, end=token.end)
-        trait.value = trait.to_int(token.groups['count'])
+        trait.value = trait.to_int(token.groups['value'])
         if token.groups.get('notation'):
             trait.notation = token.groups['notation']
         return trait
 
     @staticmethod
-    def multiple(token):
-        """Convert multiple-valued tokens into a result."""
+    def typed(token):
+        """Convert single value tokens into a result."""
         trait = NumericTrait(start=token.start, end=token.end)
-        trait.value = trait.to_int(token.groups['count'])
+        trait.notation = token.groups['notation']
+        trait.value = trait.to_int(token.groups['value1'])
+        trait.value += trait.to_int(token.groups.get('value2'))
         return trait
--- tests/trait_builders/test_nipple_count_trait_builder.py
+++ tests/trait_builders/test_nipple_count_trait_builder.py
@@ -29,7 +29,38 @@ class TestNippleCountTraitBuilder(unittest.TestCase):
             PAR.parse('6 inguinal mammae visible but small;'),
             [NumericTrait(value=6, start=0, end=17)])
 
-    # def test_parse_04(self):
-    #     self.assertEqual(
-    #         PAR.parse('mammae 2+2'),
-    #         [])
+    def test_parse_04(self):
+        self.assertEqual(
+            PAR.parse('mammae 2+2'),
+            [])
+
+    def test_parse_05(self):
+        self.assertEqual(
+            PAR.parse('0 : 2 = 4 mammae'),
+            [NumericTrait(value=4, notation='0 : 2 = 4', start=0, end=16)])
+
+    def test_parse_06(self):
+        self.assertEqual(
+            PAR.parse('mammae: 1 + 2 = 6'),
+            [NumericTrait(value=6, notation='1 + 2 = 6', start=0, end=17)])
+
+    def test_parse_07(self):
+        self.assertEqual(
+            PAR.parse('3 pec, 3 ing mammae'),
+            [NumericTrait(value=6, notation='3 pec, 3 ing', start=0, end=19)])
+
+    def test_parse_08(self):
+        self.assertEqual(
+            PAR.parse('(mammae: 1 pr + 2 pr = 6)'),
+            [NumericTrait(
+                value=6, notation='1 pr + 2 pr = 6', start=1, end=24)])
+
+    def test_parse_09(self):
+        self.assertEqual(
+            PAR.parse('4 teats exposed, mammary glands developed,'),
+            [NumericTrait(value=4, start=0, end=7)])
+
+    def test_parse_10(self):
+        self.assertEqual(
+            PAR.parse('6 conspicuous mammae;'),
+            [NumericTrait(value=6, start=0, end=20)])
