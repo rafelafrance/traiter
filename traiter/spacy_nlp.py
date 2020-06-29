a0e07f861dbc2a8041f8b416fdb4af610903c35d
rafe <raphael.lafrance@gmail.com>
2020-06-29 10:43:27 -0400

    Allow clients more freedom when setting up Spacy

--- traiter/spacy_nlp.py
+++ traiter/spacy_nlp.py
@@ -1,11 +1,11 @@
 """A wrapper around spacy so we can use our rule-based parsers."""
 
 import re
+
 import spacy
 from spacy.lang.char_classes import ALPHA, ALPHA_LOWER, ALPHA_UPPER, \
     CONCAT_QUOTES, HYPHENS, LIST_ELLIPSES, LIST_ICONS
 from spacy.tokens import Token
-
 
 Token.set_extension('data', default={})
 Token.set_extension('label', default='')
@@ -13,28 +13,36 @@ Token.set_extension('step', default=0)
 Token.set_extension('aux', default={})
 
 
-def spacy_nlp(disable=None):
+def spacy_nlp(disable=None, lang_model='en_core_web_sm', tokenizer=True):
     """A single function to build the spacy nlp object for singleton use."""
     spacy.prefer_gpu()
 
     if disable is None:
         disable = []
 
-    nlp = spacy.load('en_core_web_sm', disable=disable)
+    nlp = spacy.load(lang_model, disable=disable)
 
+    if tokenizer:
+        setup_tokenizer(nlp)
+
+    return nlp
+
+
+def setup_tokenizer(nlp):
+    """Setup custom tokenizer rules for the pipeline."""
     infix = (
-        LIST_ELLIPSES
-        + LIST_ICONS
-        + [
-            r"(?<=[0-9])[+\-\*^](?=[0-9])",
-            r"(?<=[{al}{q}])\.(?=[{au}{q}])".format(
-                al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES),
-            r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
-            # r"(?<=[{a}])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
-            r"(?<=[{a}0-9])[:<>=/+](?=[{a}])".format(a=ALPHA),
-            r"""(?:{h})+""".format(h=HYPHENS),
-            r"""[\\\[\]\(\)/:;"'+]""",
-            r"(?<=[0-9])\.?(?=[{a}])".format(a=ALPHA),  # 1.word or 1N
+            LIST_ELLIPSES
+            + LIST_ICONS
+            + [
+                r"(?<=[0-9])[+\-\*^](?=[0-9])",
+                r"(?<=[{al}{q}])\.(?=[{au}{q}])".format(
+                    al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES),
+                r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
+                # r"(?<=[{a}])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
+                r"(?<=[{a}0-9])[:<>=/+](?=[{a}])".format(a=ALPHA),
+                r"""(?:{h})+""".format(h=HYPHENS),
+                r"""[\\\[\]\(\)/:;"'+]""",
+                r"(?<=[0-9])\.?(?=[{a}])".format(a=ALPHA),  # 1.word or 1N
             ])
 
     infix_regex = spacy.util.compile_infix_regex(infix)
@@ -47,5 +55,3 @@ def spacy_nlp(disable=None):
 
     suffix = re.compile(f'{breaking}$')
     nlp.tokenizer.suffix_search = suffix.search
-
-    return nlp
