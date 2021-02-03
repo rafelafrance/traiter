45c66cd7eb3f4f9f0e5b23c92872b1142acbb6f4
rafe <raphael.lafrance@gmail.com>
2021-02-03 01:02:42 -0500

    Add a pattern compiler for dependency patterns

--- traiter/pattern_compiler.py
+++ traiter/pattern_compiler.py
@@ -10,17 +10,25 @@ some simple rules.
 from copy import deepcopy
 from warnings import warn
 
+REL_OP = ' < > << >> . .* ; ;* $+ $- $++ $-- '.split()
+
 
 class PatternCompiler:
     """Convert patterns strings to spacy token pattern arrays."""
 
-    def __init__(self, shared_patterns: dict[dict] = None):
-        self.shared = shared_patterns if shared_patterns else {}
+    def __init__(
+            self,
+            shared_patterns: dict[dict] = None,
+            shared_dependencies: dict[dict] = None
+    ):
+        self.shared_patterns = shared_patterns if shared_patterns else {}
+        self.shared_dependencies = shared_dependencies if shared_dependencies else {}
 
-    def to_patterns(self, map_: dict[str, dict], *patterns: str):
+    def to_patterns(self, map_: dict[str, dict], *patterns: str) -> list[list[dict]]:
         """Convert patterns strings to spacy token pattern arrays."""
         map_ = map_ if map_ else {}
-        map_ = {**self.shared, **map_}  # We want the arg to overwrite shared_patterns
+        # We want the arg to overwrite shared_patterns
+        map_ = {**self.shared_patterns, **map_}
 
         all_patterns = []
 
@@ -39,6 +47,53 @@ class PatternCompiler:
                     pattern_seq.append(token)
                 else:
                     warn(f'No token pattern for "{key}"')
+
+            all_patterns.append(pattern_seq)
+
+        return all_patterns
+
+    # TODO Handle branching patterns
+    def to_dependencies(
+            self, map_: dict[str, dict], *patterns: str) -> list[list[dict]]:
+        """Convert patterns strings to spacy dependency pattern arrays."""
+        map_ = map_ if map_ else {}
+        # We want the arg to overwrite shared_patterns
+        map_ = {**self.shared_dependencies, **map_}
+
+        all_patterns = []
+
+        for string in patterns:
+            pattern_seq = []
+            left_id, rel_op, right_id, right_attrs = '', '', '', {}
+
+            for i, key in enumerate(string.split()):
+
+                if key in REL_OP:
+                    rel_op = key
+
+                elif right_attrs := map_.get(key):
+                    right_id = f'{key}{i}'
+
+                    if left_id and rel_op:
+                        pattern_seq.append({
+                            'LEFT_ID': left_id,
+                            'REL_OP': rel_op,
+                            'RIGHT_ID': right_id,
+                            'RIGHT_ATTRS': right_attrs,
+                        })
+
+                    elif not (left_id or rel_op):
+                        pattern_seq.append({
+                            'RIGHT_ID': right_id,
+                            'RIGHT_ATTRS': right_attrs,
+                        })
+                    else:
+                        warn(f'Dependency patterns must go: ID op ID op ID... {string}')
+
+                    left_id = right_id
+
+                else:
+                    warn(f'No right_attrs or rel_op for "{key}"')
 
             all_patterns.append(pattern_seq)
 
--- traiter/pipes/dependency.py
+++ traiter/pipes/dependency.py
@@ -10,7 +10,7 @@ from spacy.matcher import DependencyMatcher
 from traiter.util import sign
 
 DEPENDENCY = 'dependency'
-NEAREST_LINKER = 'nearest_linker.v1'
+NEAREST_ANCHOR = 'nearest_anchor.v1'
 
 
 @Language.factory(DEPENDENCY)
@@ -71,19 +71,19 @@ class Dependency:
         return doc
 
 
-@spacy.registry.misc(NEAREST_LINKER)
-def nearest_linker(doc, matches, **kwargs):
-    """Link traits to the root trait trait.
+@spacy.registry.misc(NEAREST_ANCHOR)
+def nearest_anchor(doc, matches, **kwargs):
+    """Link traits to the nearest anchor trait.
 
     This uses a simple algorithm for linking traits.
         1) Create a set of matched entities from matches of tokens.
         2) Find all entities.
-        2) Link entities to closest root entity, favoring entities being downstream
-           being downstream of the root entity.
+        2) Link entities to closest anchor entity, favoring entities being downstream
+           of the anchor.
     """
     # print(kwargs)
     # print(matches)
-    root = kwargs.get('root')
+    anchor = kwargs.get('anchor')
     exclude = kwargs.get('exclude')
 
     # The dependency tree is built by a neural net before the linker rules are run.
@@ -92,14 +92,14 @@ def nearest_linker(doc, matches, **kwargs):
     #       may point to the same entity.
     # We want to add data to the entity not to the tokens.
     # So we need to map tokens in the matches to entities.
-    root_2_ent = array('i', [-1] * len(doc))
+    anchor_2_ent = array('i', [-1] * len(doc))
     token_2_ent = array('i', [-1] * len(doc))
 
     for i, ent in enumerate(doc.ents):
         if ent.label_ == exclude:
             continue
-        elif ent.label_ == root:
-            root_2_ent[ent.start:ent.end] = array('i', [i] * len(ent))
+        elif ent.label_ == anchor:
+            anchor_2_ent[ent.start:ent.end] = array('i', [i] * len(ent))
         else:
             token_2_ent[ent.start:ent.end] = array('i', [i] * len(ent))
 
@@ -107,9 +107,9 @@ def nearest_linker(doc, matches, **kwargs):
     root_indexes, ent_indexes = set(), set()
     for _, token_ids in matches:
         ent_indexes |= {e for t in token_ids if (e := token_2_ent[t]) > -1}
-        root_indexes |= {e for t in token_ids if (e := root_2_ent[t]) > -1}
+        root_indexes |= {e for t in token_ids if (e := anchor_2_ent[t]) > -1}
 
     # Find the closest root entity to the target entity
     for ent_idx in ent_indexes:
         nearest = sorted(root_indexes, key=lambda r: (abs(r - ent_idx), -sign(r)))[0]
-        doc.ents[ent_idx]._.data[root] = doc.ents[nearest]._.data[root]
+        doc.ents[ent_idx]._.data[anchor] = doc.ents[nearest]._.data[anchor]
