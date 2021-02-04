0621dc9a1dece74e6165be2f6356463878f6af93
rafe <raphael.lafrance@gmail.com>
2021-02-04 16:04:02 -0500

    Simplify the sentence pipe

--- traiter/pipes/sentence.py
+++ traiter/pipes/sentence.py
@@ -2,7 +2,6 @@
 
 from typing import Optional
 
-import regex
 from spacy.language import Language
 from spacy.tokens import Doc
 
@@ -10,56 +9,42 @@ SENTENCE = 'sentence'
 
 
 @Language.factory(SENTENCE)
-def sentence(
-        nlp: Language,
-        name: str,
-        abbrevs: Optional[list[str]] = None,
-        headings: Optional[list[str]] = None):
+def sentence(nlp: Language, name: str, automatic: Optional[list[str]] = None):
     """Create a sentence pipe."""
-    return Sentence(nlp, name, abbrevs, headings)
+    return Sentence(nlp, name, automatic)
 
 
 class Sentence:
     """Shared sentencizer logic."""
 
-    def __init__(self, nlp, name, abbrevs, headings):
-        """Build a custom sentencizer.
-
-        Each client sentencizer has its own abbreviations that will prevent a
-        sentence split.
-        """
+    def __init__(self, nlp, name, automatic):
+        """Build a custom sentencizer."""
         self.nlp = nlp
         self.name = name
-        abbrevs = abbrevs if abbrevs else []
-        abbrevs = '|'.join(abbrevs)
-        self.abbrevs = regex.compile(fr'(?:{abbrevs})$')
-        self.headings = headings if headings else []
+        self.automatic = automatic if automatic else []
 
     def __call__(self, doc: Doc) -> Doc:
         """Break the text into sentences."""
         for i, token in enumerate(doc[:-1]):
             prev = doc[i - 1] if i > 0 else None
             next_ = doc[i + 1]
-            # Document headings like "Introduction" are a separate sentence
-            if (token.ent_type_ in self.headings
+
+            # Some tokens are automatically their own sentence
+            if (token.ent_type_ in self.automatic
                     and (not prev or self.is_prev(prev))
                     and self.is_next(next_)):
                 next_.is_sent_start = True
                 token.is_sent_start = True
-            # A sentence if the previous token is not an abbreviation
-            elif (token.text == '.'
-                  and prev and not self.abbrevs.match(prev.text)
-                  and (len(prev) > 1 or self.is_prev(prev))
-                  and (len(next_) > 1 or self.is_next(next_))):
+
+            # A normal sentence split
+            elif token.text == '.' and self.is_prev(prev) and self.is_next(next_):
                 next_.is_sent_start = True
-            #
-            elif (token.text == '.'
-                  and self.is_prev(prev)
-                  and self.is_next(next_)):
-                next_.is_sent_start = True
+
             # Capture sentences inside of quotes
             elif token.text in '"”\'' and prev and prev.text == '.':
                 next_.is_sent_start = True
+
+            # Not a sentence break
             else:
                 next_.is_sent_start = False
 
@@ -67,7 +52,7 @@ class Sentence:
 
     @staticmethod
     def is_prev(token):
-        """See if the previous toke is a space or a bracket."""
+        """See if the previous token is a space or a bracket."""
         return token.pos_ == 'SPACE' or token.text in ')].'
 
     @staticmethod
