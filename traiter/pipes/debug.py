c15779a6f65ac4b2b9258d99077bf1ab24926866
rafe <raphael.lafrance@gmail.com>
2021-05-28 16:05:34 -0400

    Move debug message builders into the debug pipe module

--- traiter/pipes/debug.py
+++ traiter/pipes/debug.py
@@ -56,3 +56,29 @@ class DebugEntities:
             print(f'{" " * 20} {ent._.data}\n')
         print()
         return doc
+
+
+def debug_tokens(nlp, message='', **kwargs):
+    """Add pipes for debugging."""
+    global DEBUG_COUNT
+    DEBUG_COUNT += 1
+    config = {'message': message}
+    nlp.add_pipe(
+        DEBUG_TOKENS,
+        name=f'tokens_{DEBUG_COUNT}',
+        config=config,
+        **kwargs,
+    )
+
+
+def debug_ents(nlp, message='', **kwargs):
+    """Add pipes for debugging."""
+    global DEBUG_COUNT
+    DEBUG_COUNT += 1
+    config = {'message': message}
+    nlp.add_pipe(
+        DEBUG_ENTITIES,
+        name=f'entities_{DEBUG_COUNT}',
+        config=config,
+        **kwargs,
+    )
