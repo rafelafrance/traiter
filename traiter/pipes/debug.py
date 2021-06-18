da04aca17f9cb5caabae9c62a3638c378d6c7bad
rafe <raphael.lafrance@gmail.com>
2021-06-18 17:45:09 -0400

    Update debug logging headers and remove dead code

--- traiter/pipes/debug.py
+++ traiter/pipes/debug.py
@@ -13,20 +13,13 @@ add_extensions()
 DEBUG_COUNT = 0  # Used to rename debug pipes
 
 
-def rename(name: str):
-    """Add pipes for debugging."""
-    global DEBUG_COUNT
-    DEBUG_COUNT += 1
-    return f'{name}_{DEBUG_COUNT}'
-
-
 @Language.factory(DEBUG_TOKENS, default_config={'message': ''})
 class DebugTokens:
     """Print debug messages."""
 
     def __init__(self, nlp: Language, name: str, message: str = ''):
         self.nlp = nlp
-        self.name = rename(name)
+        self.name = name
         self.message = message
 
     def __call__(self, doc):
@@ -45,7 +38,7 @@ class DebugEntities:
 
     def __init__(self, nlp: Language, name: str, message: str = ''):
         self.nlp = nlp
-        self.name = rename(name)
+        self.name = name
         self.message = message
 
     def __call__(self, doc):
