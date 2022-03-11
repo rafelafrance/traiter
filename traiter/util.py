128c68dc2947f05d725f8bb7f9d970db981a4e91
rafe <raphael.lafrance@gmail.com>
2022-03-11 12:27:20 -0500

    Add the DotDict back

--- traiter/util.py
+++ traiter/util.py
@@ -20,12 +20,12 @@ INFLECT = inflect.engine()
 FLAGS = re.IGNORECASE | re.VERBOSE
 
 
-# class DotDict(dict):
-#     """Allow dot.notation access to dictionary items."""
-#
-#     __getattr__ = dict.get
-#     __setattr__ = dict.__setitem__
-#     __delattr__ = dict.__delitem__
+class DotDict(dict):
+    """Allow dot.notation access to dictionary items."""
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
 
 def setup_logger(level=logging.INFO):
