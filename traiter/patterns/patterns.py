e5d6d833142581a570e606efbc20903ff80061b5
rafe <rafelafrance@gmail.com>
2022-04-04 11:39:15 -0400

    Remove 3.10 typing union notation

--- traiter/patterns/patterns.py
+++ traiter/patterns/patterns.py
@@ -1,7 +1,8 @@
 """Common functions and data for all pattern types."""
 from typing import Any
+from typing import Union
 
 SpacyPatterns = list[list[dict[str, Any]]]
 Decoder = dict[str, dict]
-PatternArg = str | list[str] | SpacyPatterns
+PatternArg = Union[str, list[str], SpacyPatterns]
 CompilerPatterns = list[str]
