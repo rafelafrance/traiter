68e6ff57321ac4a3362d3f6ebe86329b35141fdb
rafelafrance <raphael.lafrance@gmail.com>
2022-04-12 01:24:25 -0400

    Add new pipes

--- traiter/pipes/README.md
+++ traiter/pipes/README.md
@@ -3,5 +3,6 @@
 Pipes:
 - [simple_traits](./simple_traits.py) creates traits from spacy entities by adding standard trait data to an entity.
 - [add_traits](./add_traits.py) is the workhorse pipe. Use this when you want to add custom data to a trait. It creates an entity from a matched span and fills in some standard data, but you'll need a registered function to fill in custom data.
-- [forget_traits](./delete_traits.py) is used to get rid of partially build traits.
+- [delete_traits](./delete_traits.py) is used to get rid of partially build traits.
+- [sentence](./sentence.py) breaks text into sentences using custom rules that work well on PDF files -- so far.
 - [debug_tokens & debug_entities](./debug_traits.py) are pipes that print the current state of token or entity data.
---
+++ traiter/pipes/sentence.py
@@ -0,0 +1,73 @@
+"""Custom sentence splitter."""
+from typing import Optional
+
+from spacy.language import Language
+from spacy.tokens import Doc
+
+from traiter import const
+
+SENTENCE = "traiter.sentence.v1"
+
+EOS = """ . ? ! … """.split()  # End Of Sentence
+PREV_EOS = const.CLOSE + EOS
+
+QUOTES = set(const.QUOTE)
+QUOTES.discard(",")
+
+
+@Language.factory(SENTENCE)
+class Sentence:
+    """Shared sentencizer logic."""
+
+    def __init__(self, nlp: Language, name: str, automatic: Optional[list[str]] = None):
+        """Build a custom sentencizer."""
+        self.nlp = nlp
+        self.name = name
+        self.automatic = automatic if automatic else []
+
+    def __call__(self, doc: Doc) -> Doc:
+        """Break the text into sentences."""
+        for i, token in enumerate(doc[:-1]):
+            prev = doc[i - 1] if i > 0 else None
+            next_ = doc[i + 1]
+
+            # Some tokens are automatically their own sentence
+            if (
+                token.ent_type_ in self.automatic
+                and (not prev or self.is_prev(prev))
+                and self.is_next(next_)
+            ):
+                next_.is_sent_start = True
+                token.is_sent_start = True
+
+            # A period followed by a capital letter (or space, digit or another period)
+            elif token.text == "." and self.is_next(next_):
+                next_.is_sent_start = True
+
+            # Quotes preceded by a period
+            elif token.text in QUOTES and prev and prev.text in EOS:
+                next_.is_sent_start = True
+
+            # Not a sentence break
+            else:
+                next_.is_sent_start = False
+
+        return doc
+
+    @staticmethod
+    def is_space(token):
+        """Check if the token is space."""
+        return token.text.isspace() or token.pos_ == "SPACE"
+
+    def is_prev(self, token):
+        """See if the previous token is a space or a bracket."""
+        return self.is_space(token) or token.text in PREV_EOS
+
+    def is_next(self, token):
+        """See if the next token starts with an uppercase is a space or period."""
+        return (
+            token.prefix_.isupper()
+            or token.prefix_.isdigit()
+            or self.is_space(token)
+            or token.text in EOS
+        )
