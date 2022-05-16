aabdba9a9faf9756e513a9a767832f448e7306e5
rafelafrance <raphael.lafrance@gmail.com>
2022-05-16 10:19:34 -0400

    Simplify sentence pipe to increase speed

--- traiter/pipes/sentence_pipe.py
+++ traiter/pipes/sentence_pipe.py
@@ -21,30 +21,19 @@ class Sentence:
         self,
         nlp: Language,
         name: str,
-        automatic: Optional[list[str]] = None,
         abbrev: Optional[list[str]] = None,
     ):
         self.nlp = nlp
         self.name = name
         self.abbrev = set(abbrev) if abbrev else set()
-        self.automatic = automatic if automatic else []
 
     def __call__(self, doc: Doc) -> Doc:
         for i, token in enumerate(doc[:-1]):
             prev = doc[i - 1] if i > 0 else None
             next_ = doc[i + 1]
 
-            # Some tokens are automatically their own sentence
-            if (
-                token.ent_type_ in self.automatic
-                and (not prev or self.is_prev(prev))
-                and self.is_next(next_)
-            ):
-                next_.is_sent_start = True
-                token.is_sent_start = True
-
             # A period followed by a capital letter (or space, digit or another period)
-            elif (
+            if (
                 token.text.endswith(".")
                 and self.is_next(next_)
                 and token.text not in self.abbrev
@@ -65,10 +54,6 @@ class Sentence:
     def is_space(token):
         """Check if the token is space."""
         return token.text.isspace() or token.pos_ == "SPACE"
-
-    def is_prev(self, token):
-        """See if the previous token is a space or a bracket."""
-        return self.is_space(token) or token.text in PREV_EOS
 
     def is_next(self, token):
         """See if the next token starts with an uppercase is a space or period."""
