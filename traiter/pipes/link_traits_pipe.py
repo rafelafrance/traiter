0f8591218c8300e35e335d4edc70f660a0fe8dc8
rafelafrance <raphael.lafrance@gmail.com>
2022-07-12 08:36:33 -0400

    Allow a limit on how many child traits link to a parent trait

--- traiter/pipes/link_traits_pipe.py
+++ traiter/pipes/link_traits_pipe.py
@@ -1,4 +1,4 @@
-from itertools import groupby
+from collections import defaultdict
 
 from spacy.language import Language
 from spacy.matcher import Matcher
@@ -7,6 +7,43 @@ from spacy.tokens import Doc
 from traiter.pipes import pipe_util
 
 LINK_TRAITS = "traiter.link_traits.v1"
+
+NO_LIMIT = 999_999
+
+
+class LinkMatch:
+    def __init__(self, span, weights, doc, parents):
+        self.span = span
+        self.doc = doc
+        self.weights = weights
+        self.parents = parents
+        self.distance = self.weighted_distance()
+        self.child_idx, self.parent_idx = self.get_indices()
+        self.child_ent = self.get_ent_from_token(self.child_idx)
+        self.parent_ent = self.get_ent_from_token(self.parent_idx)
+        self.child_trait = self.child_ent._.data["trait"]
+        self.parent_trait = self.parent_ent._.data["trait"]
+
+    def __lt__(self, other):
+        return self.distance < other.distance
+
+    def __len__(self):
+        return len(self.span)
+
+    def get_ent_from_token(self, token_idx):
+        return next(e for e in self.doc.ents if e.start <= token_idx < e.end)
+
+    def get_indices(self):
+        child_idx, parent_idx = self.span.start, self.span.end - 1
+        if self.span[0].ent_type_ in self.parents:
+            child_idx, parent_idx = parent_idx, child_idx
+        return child_idx, parent_idx
+
+    def weighted_distance(self):
+        return sum(
+            self.weights.get(self.doc[i].lower_, 1)
+            for i in range(self.span.start, self.span.end)
+        )
 
 
 @Language.factory(LINK_TRAITS)
@@ -18,7 +55,9 @@ class LinkTraits:
         parents: list[str],
         children: list[str],
         patterns: list[dict],
-        weights: dict[str, int] = None,
+        weights: dict[str, int] = None,  # Token weights for scoring distances
+        max_links: int = NO_LIMIT,  # Max times to link to a parent trait
+        differ: list[str] = None,
     ):
         pipe_util.add_extensions()
 
@@ -27,6 +66,8 @@ class LinkTraits:
         self.parents = parents
         self.children = children
         self.weights = {k.lower(): v for k, v in weights.items()} if weights else {}
+        self.max_links = max_links
+        self.differ = differ if differ else []
         self.matcher = self.build_matcher(nlp, patterns)
 
     @staticmethod
@@ -38,54 +79,34 @@ class LinkTraits:
 
     def __call__(self, doc: Doc) -> Doc:
         matches = self.matcher(doc, as_spans=True)
-        matches = self.filter_matches(doc, matches)
+        matches = [LinkMatch(m, self.weights, doc, self.parents) for m in matches]
+        matches = sorted(matches)
 
-        for span in matches:
-            if len(span) <= 1:
+        parent_link_count = defaultdict(int)
+        child_link_count = defaultdict(int)
+
+        for match in matches:
+            if len(match) <= 1:
                 continue
 
-            # Get the parent and child locations
-            if span[0].ent_type_ in self.parents:
-                t_idx, p_idx = span.end - 1, span.start
-            else:
-                t_idx, p_idx = span.start, span.end - 1
+            # See if the link limit will be exceeded
+            key = [match.parent_idx, match.child_trait]
+            key += [match.child_ent._.data.get(d, "") for d in self.differ]
+            key = tuple(key)
+            parent_link_count[key] += 1
+            if parent_link_count[key] > self.max_links:
+                continue
 
-            # Get the parent's trait
-            parent = self.get_ent_from_token(doc, doc[p_idx].i)
-            parent_trait = parent._.data["trait"]
+            # See if this trait is already linked
+            key = (match.child_idx, match.parent_trait)
+            child_link_count[key] += 1
+            if child_link_count[key] > 1:
+                continue
 
             # Update the child entity and token
-            ent = self.get_ent_from_token(doc, doc[t_idx].i)
-            ent._.data[parent_trait] = doc[p_idx]._.data[parent_trait]
-            doc[t_idx]._.data[parent_trait] = ent._.data[parent_trait]
+            if match.parent_trait in match.parent_ent._.data:
+                parent_trait = match.parent_ent._.data[match.parent_trait]
+                match.child_ent._.data[match.parent_trait] = parent_trait
+                doc[match.child_idx]._.data[match.parent_trait] = parent_trait
 
         return doc
-
-    @staticmethod
-    def get_ent_from_token(doc, token_idx):
-        return next(e for e in doc.ents if e.start <= token_idx < e.end)
-
-    def filter_matches(self, doc, matches):
-        # Find the index of the child token -- it's at one end or the other
-        idx = [
-            m.end - 1 if m[0].ent_type_ in self.parents else m.start for m in matches
-        ]
-        if self.weights:
-            distances = self.weighted_distances(doc, matches)
-        else:
-            distances = [m.end - m.start for m in matches]
-        matches = zip(idx, distances, matches)
-        matches = sorted(matches)
-        grouped = groupby(matches, key=lambda m: m[0])
-        matches = [list(g)[0][2] for _, g in grouped]
-        return matches
-
-    def weighted_distances(self, doc, matches):
-        distances = []
-        for match in matches:
-            dist = 0
-            for idx in range(match.start, match.end):
-                token = doc[idx]
-                dist += self.weights.get(token.lower_, 1)
-            distances.append(dist)
-        return distances
