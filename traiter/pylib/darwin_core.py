336b0874ea91b9af97691cce38a985cd8a869feb
rafelafrance <raphael.lafrance@gmail.com>
2023-11-03 12:48:46 -0400

    Remove duplicate values from dwc output

--- traiter/pylib/darwin_core.py
+++ traiter/pylib/darwin_core.py
@@ -46,19 +46,24 @@ class DarwinCore:
             return values if len(values) > 1 else values[0]
 
         if all(isinstance(v, dict) for v in values):
-            return SEP.join(json.dumps(v) for v in values)
+            new_list = []
+            for val in values:
+                new = ", ".join(f"{k}: {v}" for k, v in val.items())
+                new_list.append(new)
+            return SEP.join(json.dumps(v) for v in new_list)
 
         raise ValueError(f"Field: {key} has mixed value types: {values=}")
 
     def add(self, **kwargs) -> "DarwinCore":
         for key, value in kwargs.items():
-            if value is not None:
-                self.props[self.ns(key)].append(value)
+            key = self.ns(key)
+            if value is not None and value not in self.props[key]:
+                self.props[key].append(value)
         return self
 
     def add_dyn(self, **kwargs) -> "DarwinCore":
         for key, value in kwargs.items():
-            if value is not None:
+            if value is not None and value not in self.dyn_props[key]:
                 self.dyn_props[key].append(value)
         return self
 
